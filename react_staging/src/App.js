a696f1c0d5a802cd318950293938d8dbf5b081cb
lwl <lwl380451663@qq.com>
2021-12-08 21:47:18 +0800

    学习react第18天(github搜索案例完成)

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -3,11 +3,24 @@ import Search from './components/Search'
 import List from './components/List'
 
 export default class App extends Component {
+
+  state = {
+    users: [], //初始化状态 users初始值为[]
+    isFirst: true, //是否为第一次打开页面
+    isLoading: false, //是否加载中
+    err: '' //存储请求相关的错误信息
+  }
+
+  updateAppState = (stateObj) => {
+    this.setState(stateObj)
+  }
+
   render() {
+    const { users } = this.state
     return (
       <div className="container">
-        <Search/>
-        <List/>
+        <Search updateAppState={this.updateAppState} />
+        <List {...this.state} />
       </div>
     )
   }
--- react_staging/src/components/List/index.jsx
+++ react_staging/src/components/List/index.jsx
@@ -3,38 +3,25 @@ import './index.css'
 
 export default class List extends Component {
   render() {
+    const { users, isFirst, isLoading, err } = this.props
     return (
       <div className="row">
-        <div className="card">
-          <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-            <img alt="head_portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-            <img alt="head_portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-            <img alt="head_portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-            <img alt="head_portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
-            <img alt="head_portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
+        {
+          isFirst ? <h2>欢迎使用，请输入关键字，随后点击搜索</h2> :
+            isLoading ? <h2>Loading......</h2> :
+              err ? <h2 style={{ color: 'red' }}>{err}</h2> :
+                users.length <= 0 ? <h2>暂无数据</h2> :
+                  users.map(userObj => {
+                    return (
+                      <div className="card" key={userObj.id}>
+                        <a rel="noreferrer" href={userObj.html_url} target="_blank">
+                          <img alt="head_portrait" src={userObj.avatar_url} style={{ width: '100px' }} />
+                        </a>
+                        <p className="card-text">{userObj.login}</p>
+                      </div>
+                    )
+                  })
+        }
       </div>
     )
   }
