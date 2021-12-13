5249ea9f7a933698171594d88b4d96c1e998e4c9
lwl <lwl380451663@qq.com>
2021-12-13 14:48:29 +0800

    学习react第20天(路由初识)

--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -2,7 +2,15 @@
 import React from 'react'
 //引入ReactDOM
 import ReactDOM from 'react-dom';
+//
+import { BrowserRouter } from 'react-router-dom'
+
 //引入app
 import App from './App'
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+)
